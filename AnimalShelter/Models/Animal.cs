5ff47257b9084350858cd532525ea1cab2f11e5c
Benjamin Thom <jaminthom@gmail.com>
2020-03-17 14:56:40 -0700

    add fields to animal class
    Co-authored-by: Drake-Wilcox <drakewilcox@gmail.com>
    Co-authored-by: Todd Walraven <cod4247@yahoo.com>

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,8 +1,10 @@
-namespace AnimalShelter.Models
-{
-  public class Animal
-  {
+namespace AnimalShelter.Models {
+  public class Animal {
     public int AnimalId { get; set; }
     public string Type { get; set; }
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public string Breed { get; set; }
+    public int Date { get; set; }
   }
 }
